e35d880736b658c1f67c8314a0ee53c91694dd80
Marian1017l <mariana.lopez35806@ucaldas.edu.co>
2024-12-13 00:02:29 -0500

    Se creó el modelo de usuario}

---
+++ src/models/usuario.model.ts
@@ -0,0 +1,51 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Usuario extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  idUsuario?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  correo: string;
+
+  @property({
+    type: 'string',
+  })
+  telefono?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  estado: boolean;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  fechaRegistro: string;
+
+
+  constructor(data?: Partial<Usuario>) {
+    super(data);
+  }
+}
+
+export interface UsuarioRelations {
+  // describe navigational properties here
+}
+
+export type UsuarioWithRelations = Usuario & UsuarioRelations;
